0aaeaa402fffed1f4490c5b8a1085de8877195bc
EgorLomonov <32848001+EgorLomonov@users.noreply.github.com>
2025-04-07 02:13:59 +0300

    ке

--- js/script.js
+++ js/script.js
@@ -1,4 +1,40 @@
 "use strict";
+
+
+  var materialParallax = $(".parallax-container"),
+      userAgent = navigator.userAgent.toLowerCase(),
+      isIE = userAgent.indexOf("msie") !== -1 ? parseInt(userAgent.split("msie")[1], 10) : userAgent.indexOf("trident") !== -1 ? 11 : userAgent.indexOf("edge") !== -1 ? 12 : false,
+      isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+/**
+ * Initialize All Scripts
+ */
+$document.ready(function () {
+    var isNoviBuilder = window.xMode;
+  /**
+       * Material Parallax
+       * @description Enables Material Parallax plugin
+       */
+      if (materialParallax.length) {
+        var i;
+  
+        if (!isNoviBuilder && !isIE && !isMobile) {
+          materialParallax.parallax();
+        } else {
+          for (i = 0; i < materialParallax.length; i++) {
+            var parallax = $(materialParallax[i]),
+              imgPath = parallax.data("parallax-img");
+  
+            parallax.css({
+              "background-image": 'url(' + imgPath + ')',
+              "background-attachment": "fixed",
+              "background-size": "cover"
+            });
+          }
+        }
+      }
+});
+
 (function () {
 	// Global variables
 	var
